e140985cc54fa222509dd26cb744f8a14f91c681
benily <leekwokhinben@gmail.com>
2022-01-22 16:57:44 +0800

    add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import infoReducer from '../src/store/reducers/info';
 import '../styles/globals.css';
 
+const rootReducer = combineReducers({
+  info: infoReducer,
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ src/store/reducers/info.js
@@ -0,0 +1,10 @@
+const initState = { name: 'Hello' };
+
+const infoReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default infoReducer;
