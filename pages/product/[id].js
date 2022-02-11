9e31ebc04da269b7bb8190f100d910747d4f8d0d
benily <leekwokhinben@gmail.com>
2022-02-11 09:58:33 +0800

    update

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 
 import MOCK_DATA from '../../constants/MOCK_DATA';
+import Layout from '../../src/components/Layout';
 
 const ProductDetails = () => {
   const router = useRouter();
@@ -8,12 +9,12 @@ const ProductDetails = () => {
   const item = MOCK_DATA.find((ele) => ele.id == id);
 
   return (
-    <>
+    <Layout>
       <h1>Product details of {id}</h1>
       <h2>Price: ${item.price}</h2>
       <h2>Rating: {item.rating} / 100</h2>
       <h2>{item.description}</h2>
-    </>
+    </Layout>
   );
 };
 
